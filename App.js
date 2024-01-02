ccba72681a2cf52ac1267916971a95284bc47525
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2024-01-02 13:31:55 +0530

    refactor - alignment of code

--- App.js
+++ App.js
@@ -1,27 +1,31 @@
+// 1. Define the API endpoint URL for the translation service
+const serverApi = "https://api.funtranslations.com/translate/shakespeare.json";
+
+// get refference of html element by using id 
 const translateBtn = document.querySelector("#translate-btn");
 const textTranslateBox = document.querySelector("#textTranslateBox");
 const showOutputMessage = document.querySelector("#ouput");
 
-const serverApi = "	https://api.funtranslations.com/translate/shakespeare.json";
 const translateServerApi = (text) => {
   return serverApi + "?" + "text=" + text;
 };
 
+// error handling 
 function errorHandler(error) {
   console.log("error occured " + error);
   alert("something wrong with server try again after some time");
 }
 
 translateBtn.addEventListener("click", () => {
-  /* taking input via translate box */ 
-  const textInputQuerry = textTranslateBox.value;
-
-  /* processing input that took */
-  fetch(translateServerApi(textInputQuerry))
-    .then((res) => res.json())
-    .then((json) => {
-      let translate = json.contents.translated;
-      showOutputMessage.innerText = translate;
+//get the user input form the user 
+const textInputQuerry = textTranslateBox.value;
+// perform fetch request 
+fetch(translateServerApi(textInputQuerry))
+// parse a response as json 
+  .then((response) => response.json())
+  .then((json) => {
+   let translate = json.contents.translated;
+   showOutputMessage.innerText = translate;
     })
-    .catch(errorHandler);
+  .catch(errorHandler);
 });
